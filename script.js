dd00b72ac26f679a3f6f5a7625dd19f9528acde8
Vita Fomin <vita.bukhalo@gmail.com>
2023-03-22 18:39:23 -0400

    added a click event

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+var container = $(".time-block");
+var description = $(".description");
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
+
+  container.on("click", ".saveBtn", function() {
+    var descriptionEvent = description.val();
+
+    localStorage.setItem("descriptionEvent", JSON.stringify());
+  })
+
+
+
+
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
